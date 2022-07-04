da1d451489fc7686ab1f3c200281c18ed03ba593
castillomiguel25 <miguelangelcastillo947@gmail.com>
2022-07-04 15:57:30 -0400

    init

---
+++ src/js/save.example.js
@@ -0,0 +1,3 @@
+// Crea por favor tu archivo Secret.js con tu API KEY
+
+const API_KEY = '';
---
+++ src/js/style.js
@@ -0,0 +1,3 @@
+async function getTrendingMoviesPreview() {
+    const res = away fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
+}
